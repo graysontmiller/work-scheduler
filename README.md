503e30a66df239c20e1b072d231d3ec5c3340afd
Grayson Miller <graysonmiller@Graysons-MacBook-Pro.local>
2022-04-14 22:14:07 -0400

    workday scheduler

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# work-scheduler
+# Work Scheduler
+
+## Description 
+
+This color coded work-day schedule helps the user plan their day ahead. The 9-5 work day is depicted in time passed (grey), future time (green), and the present (red).
+
+
+### Technology used 
+- third-party libraries (bootstrap, jquery and moment.js)
+- DOM traversals and manipulations with jquery
+- save user input into local storage with jquery
+- Used Bootstrap grid for the HTMl layout
+
+(local storage is still being worked on)
+
+Deployed page: https://jmail1014.github.io/work-scheduler/
+
+![work-scheduler-screenshot](https://user-images.githubusercontent.com/45181939/144946002-523ffc17-39b4-4c88-9b60-0b43c0ea1481.png)
---
+++ script.js
@@ -0,0 +1,37 @@
+ // current time
+ var currentTime = moment().format('H');
+ console.log(currentTime);
+ // current date display
+ var currentDate = moment().format("dddd, MMM Do YYYY");
+ 
+ $("#currentDay").text(currentDate);
+
+ $( ".time-block" ).each(function() {
+
+   var id = $( this ).attr("id");
+   console.log(id, currentTime);
+
+   // color code textarea to represent past, present or future
+   if (currentTime === id) {
+     $(this).children('textarea').addClass('present');
+   } else if (id > currentTime) {
+     $(this).children('textarea').addClass('future');
+   } else if (id < currentTime) {
+     $(this).children('textarea').addClass('past');
+   }
+ });
+
+ $(".saveBtn").click(saveClick);
+
+ function saveClick() {
+   var textArea = $(this).siblings("textarea").val();
+   var hourEl = $(this).parent().attr("id");
+
+   localStorage.setItem(hourEl, textArea);
+   return localStorage.getItem(hourEl);
+ };
+
+
+ // $(".description").on("click","textarea", function() {
+ 
+ // });
